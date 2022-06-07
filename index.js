66f2da3a0d92ff99d0fe9583b30f6bda9641cece
Ephrem-Adugna <ephremadugna1219@gmail.com>
2022-06-07 15:19:51 -0700

    Fix New Line problem

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ document.getElementById('conv').addEventListener('click', function () {
     cstext.value = "";
     switch (dropdown) {
         case '0':
-    for (var elem of sqltext.split('\n')) {
-        var name = elem.split('\t')[0];
-        var dtype = elem.split('\t')[1];
-        if (dtype.includes('varchar')) {
-            dtype = 'string';
-        }
-        else if (dtype.includes('bit')) {
-            dtype = 'bool';
-        }
-        else if (dtype.includes('date')) {
-            dtype = 'DateTime';
-        }
-        cstext.value += `\npublic ${dtype} ${name} { get; set; }`;
+            for (var elem of sqltext.split('\n')) {
+                var name = elem.split('\t')[0];
+                var dtype = elem.split('\t')[1];
+                if (name.length < 3 || dtype.length < 3) {
+                    continue;
+                }
+                if (dtype.includes('varchar')) {
+                    dtype = 'string';
+                }
+                else if (dtype.includes('bit')) {
+                    dtype = 'bool';
+                }
+                else if (dtype.includes('date')) {
+                    dtype = 'DateTime';
+                }
+                cstext.value += `\npublic ${dtype} ${name} { get; set; }`;
             }
+            break;
         case '1':
+            sqltext = cstext.value;
             cstext.value = 'public class ' + sqltext.split(' ')[2] + ' { \n';
             for (var elem of sqltext.replace('\n', '').replace('\t', '').split('(')[1].split(',')) {
                 var elements = elem.replace(/^\s+|\s+$/g, '').split(' ');
